0c6f9abf2bd2996fe47b4fe1490a963ab6dc0a9e
nhattran1206 <s3924826@rmit.edu.vn>
2023-09-04 14:37:21 +0700

    Create product.model.js

---
+++ backend/app/models/product.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+// Define a Mongoose schema for the product
+const productSchema = mongoose.model(
+    "productSchema",
+    new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 20,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  image: {
+    type: String,
+  },
+  description: {
+    type: String,
+    maxlength: 500,
+  },
+})
+);
+
+module.exports = productSchema;
