400343e958b7a30181777c9d3e57ea9d84cb5ddb
nhattran1206 <s3924826@rmit.edu.vn>
2023-09-05 08:58:25 +0700

    fixed EADDRINUSE: address already in use :::8080

--- backend/app/routes/product.routes.js
+++ backend/app/routes/product.routes.js
@@ -1,25 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
 const productController = require('../controllers/product.controller'); // Import the controller
 
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-
-// Connect to your MongoDB database
-mongoose.connect('mongodb://localhost/your_database_name', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
 // Routes
+module.exports = function (app) {
 app.post('/products', productController.createProduct);
 app.get('/products', productController.getAllProducts);
 app.get('/products/:id', productController.getProductById);
 app.put('/products/:id', productController.updateProductById);
 app.delete('/products/:id', productController.deleteProductById);
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+};
