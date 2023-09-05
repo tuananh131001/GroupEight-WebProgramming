9aa542196160800b80507b3289846076376113fa
nhattran1206 <s3924826@rmit.edu.vn>
2023-09-05 09:47:44 +0700

    add check role for  vendor

--- backend/app/routes/product.routes.js
+++ backend/app/routes/product.routes.js
@@ -1,11 +1,12 @@
+const checkUserRole = require('./middleware/checkUserRole');
 const productController = require('../controllers/product.controller'); // Import the controller
 
 // Routes
 module.exports = function (app) {
-app.post('/products', productController.createProduct);
+app.post('/products', checkUserRole('vendor'), productController.createProduct);
 app.get('/products', productController.getAllProducts);
 app.get('/products/:id', productController.getProductById);
-app.put('/products/:id', productController.updateProductById);
-app.delete('/products/:id', productController.deleteProductById);
+app.put('/products/:id', checkUserRole('vendor'), productController.updateProductById);
+app.delete('/products/:id', checkUserRole('vendor'), productController.deleteProductById);
 app.get('/products/filter', productController.getFilteredProducts);
 };
