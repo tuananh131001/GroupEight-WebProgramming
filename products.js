f440385d232c85daef6fcaba6e1722c2a8f867d1
Quan317 <116570837+Quan317@users.noreply.github.com>
2023-09-08 00:27:02 +0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        image: "p-1.jpg",
+        name: "Fairtrade Pocket Hoodie",
+        price: 64.95,
+        description: "A comfortable and stylish hoodie made from fairtrade materials."
+    },
+    {
+        id: 2,
+        image: "p-2.jpg",
+        name: "Fairtrade Zip Hoodie",
+        price: 59.95,
+        description: "A warm and durable zip-up hoodie made with fairtrade cotton."
+    },
+    {
+        id: 3,
+        image: "p-3.jpg",
+        name: "Rudby Jergy",
+        price: 69.95,
+        description: "A soft and breathable jersey made with high-quality Rudby cotton."
+    }
+];
+
+module.exports = products;
