054823a5c4a37eb8223ceb3db6603664eead27cf
nhattran1206 <s3924826@rmit.edu.vn>
2023-09-22 19:39:31 +0700

    Update shoppingCart.js

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -12,10 +12,18 @@
         shoppingCart.push(orderedItem);
 
         // Calculate the total price
-        let totalPrice = 0;
-        shoppingCart.forEach(item => {
-            totalPrice += item.price;
-        });
+        updateTotalPrice();
+
+        // Update the shopping cart display
+        updateShoppingCartDisplay();
+    }
+
+    function removeFromCart(index) {
+        // Remove the item from the shopping cart based on its index
+        shoppingCart.splice(index, 1);
+
+        // Calculate the total price
+        updateTotalPrice();
 
         // Update the shopping cart display
         updateShoppingCartDisplay();
@@ -23,22 +31,25 @@
 
     function updateShoppingCartDisplay() {
         const cartList = document.querySelector('#cart-list');
-        const cartTotal = document.querySelector('#cart-total');
+        cartList.innerHTML = ''; // Clear previous cart items
 
-        // Clear the previous cart content
-        cartList.innerHTML = '';
-
-        // Populate the cart with items
-        shoppingCart.forEach(item => {
+        shoppingCart.forEach((item, index) => {
             const listItem = document.createElement('li');
-            listItem.textContent = `${item.name} - $${item.price}`;
+            listItem.innerHTML = `${item.name} - $${item.price} <button type="button" onclick="removeFromCart(${index})">Remove</button>`;
             cartList.appendChild(listItem);
         });
+    }
 
-        // Update the total price
+    function updateTotalPrice() {
+        let totalPrice = 0;
+        shoppingCart.forEach(item => {
+            totalPrice += item.price;
+        });
+
+        const cartTotal = document.querySelector('#cart-total');
         cartTotal.textContent = `Total: $${totalPrice}`;
     }
-    
+
     function submitOrder() {
         // Create an array of ordered items with product name and price
         const orderedItems = shoppingCart.map(item => ({
