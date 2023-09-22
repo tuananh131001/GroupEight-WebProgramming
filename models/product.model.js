104741a82e25598a8627b7edbaec314d67c47d67
nhattran1206 <s3924826@rmit.edu.vn>
2023-09-22 19:32:56 +0700

    change image type to string

--- models/product.model.js
+++ models/product.model.js
@@ -16,9 +16,7 @@ const productSchema = mongoose.model(
     min: 0,
   },
   image: {
-    data: Buffer, // Store binary data of the image
-    contentType: String, // Store the content type (e.g., 'image/jpeg', 'image/png')
-    // filename: String,
+    type: String,
   },
   description: {
     type: String,
